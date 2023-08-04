5d3acb03e740e36658b46309a68a45b92f15e1e7
jiashengdong <jsd957710425@163.com>
2023-08-04 16:17:59 +0800

    init

---
+++ eureka-server/src/main/java/org/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+@ComponentScan("org.jiashengdong")
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
